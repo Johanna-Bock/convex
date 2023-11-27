a3a229e884a42f8c8ee802ab042c2ea07f6f7325
Johanna Bock <johanna.bock1@stud-mail.uni-wuerzburg.de>
2023-11-27 23:23:20 +0100

    api connection

--- convex/app/page.tsx
+++ convex/app/page.tsx
@@ -14,13 +14,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className ="flex flex-col">
-        <div className=" bg-white rounded-xl h-[400px] w-[200px} nb-2"></div>
+        <div className="text-black bg-white rounded-xl h-[400px] w-[200px} nb-2"></div>
         
         <form onSubmit={(e) =>{
           e.preventDefault();
           handlePlayerAction({ message });
         }}> 
-          <input name="message" value={message} onChange={(e) => setMessage(e.target.value)}
+          <input 
+          className='p-1 rounded text-black'
+          name="message" 
+          value={message} 
+          onChange={(e) => setMessage(e.target.value)}
           />
           <button>Submit</button>
         </form>
--- convex/convex/chat.ts
+++ convex/convex/chat.ts
@@ -2,12 +2,22 @@
 
 import { v } from "convex/values";
 import { action } from "./_generated/server";
+import OpenAI from 'openai';
+
+const openai = new OpenAI();
 
 export const handlePlayerAction = action({
     args: {
         message: v.string(),
     },
-  handler: () => {
-    return "success";
+  handler: async (ctx, args) => {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: args.message}],
+        model: 'gpt-3.5-turbo',
+      });
+
+      console.log(completion);
+
+    return completion;
   },
 });
